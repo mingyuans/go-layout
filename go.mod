0bdb70ffc3a28361c1c434ec1d9910bb81f92a32
yxq <xq.yan@aftership.com>
2021-11-25 12:27:23 +0800

    feat: added validation

--- go.mod
+++ go.mod
@@ -94,6 +94,7 @@ require (
 	golang.org/x/text v0.3.6 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/go-playground/validator.v9 v9.31.0 // indirect
 	gopkg.in/ini.v1 v1.63.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gorm.io/gorm v1.21.12 // indirect
--- internal/iam-apiserver/controller/v1/users/get.go
+++ internal/iam-apiserver/controller/v1/users/get.go
@@ -1,18 +1,29 @@
 package controller_v1_user
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/marmotedu/errors"
+	"github.com/mingyuans/go-layout/internal/pkg/code"
 	"github.com/mingyuans/go-layout/internal/pkg/server"
 	"github.com/mingyuans/go-layout/pkg/log"
 	"go.uber.org/zap"
+	"gopkg.in/go-playground/validator.v9"
 )
 
 // Get GET users/userid
 func (u *UserController) Get(c *gin.Context) {
-	log.L(c).Info("Get user function called.")
-
 	userid := c.Param("userid")
 	log.L(c).Info("Get user.", zap.String("userid", userid))
+
+	errs := validator.New().Var(userid,"required,len=32")
+	if errs != nil {
+		fmt.Println(errs)
+		server.NewRestfulResponseBuilder(c).
+			Error(errors.WithCode(code.ErrValidation,"The user id {%s} is invalid.",userid)).
+			SendJSON()
+		return
+	}
 
 	user, err := u.srv.Users().Get(c, "sss")
 	server.NewRestfulResponseBuilder(c).

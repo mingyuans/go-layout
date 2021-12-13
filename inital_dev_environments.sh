29788a39da664d60b27d0e8e8983ec8389c028cf
yxq <yanxiangqunwork@163.com>
2021-12-14 00:24:19 +0800

    feat: remove useless file

--- inital_dev_environments.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Install go-gitlint, we use it to check commit message.
-# If you don't install this tool, unfortunately you can't submit anythings.
-go get github.com/llorllale/go-gitlint/cmd/go-gitlint
